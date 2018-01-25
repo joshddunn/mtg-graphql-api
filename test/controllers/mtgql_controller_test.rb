852a23e77d29f569ccd7357a167d8cdce5a72a21
Josh Dunn <joshdunn.uwo@gmail.com>
2018-01-24 21:47:40 -0500

    route tests

--- test/controllers/mtgql_controller_test.rb
+++ test/controllers/mtgql_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class MtgqlControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can access get /v1" do
+    get v1_url
+    assert_response :success
+  end
+
+  test "can access post /v1" do
+    post v1_url
+    assert_response :success
+  end
 end
